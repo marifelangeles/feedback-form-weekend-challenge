4ac7f52a05961984fa2859454d6dd2dbfd4d1d01
Marifel Angeles <mmarifel@gmail.com>
2019-03-01 15:26:32 -0600

    connect to redux

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 import './App.css';
+import {HashRouter as Router, Route} from 'react-router-dom';
+import {connect} from 'react-redux';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Feedback!</h1>
-          <h4><i>Don't forget it!</i></h4>
+        <header>
+          <h1>Feedback!</h1>
+          <p>Don't forget it!</p>
         </header>
-        <br/>
+        <Router>
+          
+        </Router>
       </div>
     );
   }
 }
 
-export default App;
+const mapReduxStateToProps = (reduxState) => {
+  return reduxState;
+}
+export default connect(mapReduxStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
-import registerServiceWorker from './registerServiceWorker';
+import {createStore, combineReducers} from 'redux';
+import {Provider} from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+const storeInstance = createStore( 
+    combineReducers({
+        
+    })
+)
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
