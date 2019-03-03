5674144318c4799610d33d94fbd48298d3cec2db
Marifel Angeles <mmarifel@gmail.com>
2019-03-03 16:44:45 -0600

    add admin header and table

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -7,7 +7,31 @@ class Admin extends Component {
     render() {
         return (
             <div>
-                Admin
+                <header>
+                    <h1>Feedback Results!</h1>
+                </header>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Feeling</th>
+                            <th>Comprehension</th>
+                            <th>Support</th>
+                            <th>Comments</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>feeling</td>
+                            <td>understanding</td>
+                            <td>support</td>
+                            <td>Feedback description</td>
+                            <td>
+                                <button>Delete</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         );
     }
