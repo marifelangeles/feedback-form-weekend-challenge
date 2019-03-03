8b48a33287d0f9dc58532ed805ecc78b466f8746
Marifel Angeles <mmarifel@gmail.com>
2019-03-03 10:54:02 -0600

    add pool query to GET /feedback

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -2,8 +2,15 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
 
+
 router.get('/', (req, res) => {
-    console.log('Get request hit');
-    res.send('Get request hit');
+    console.log('GET /feedback');
+    pool.query(`SELECT * FROM "feedback";`)
+        .then(result => {
+            res.send(result.rows);
+        }).catch(error => {
+            console.log('error with GET /feedback', error);
+            res.sendStatus(500);
+        })
 })
 module.exports = router;
